36e493d2cf0650fbe820c992cc1063370a42a2fd
Marcin <m.krolczyk66@gmail.com>
2020-10-27 13:31:31 +0100

    added builder to process class and changed local db

--- src/main/java/io/github/organizationApp/expensesProcess/Process.java
+++ src/main/java/io/github/organizationApp/expensesProcess/Process.java
@@ -13,19 +13,19 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "expenses_process")
-@JsonIgnoreProperties({"id","category","ownerId"})
+@JsonIgnoreProperties({"category","ownerId"})
 public class Process extends RepresentationModel<Process> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "description can't be null or empty")
+//    @NotBlank(message = "description can't be null or empty")
     private String description;
-    @NotNull(message = "price can't be null or empty")
+//    @NotNull(message = "price can't be null or empty")
     private BigDecimal price;
-    @NotBlank(message = "select currency")
+//    @NotBlank(message = "select currency")
     private String currency;
     private String transaction_type;
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime buy_date;
     private String notes;
     @Column(name = "owner_id")
@@ -47,39 +47,39 @@ public class Process extends RepresentationModel<Process> {
     /**
      * Hibernate use it
      */
-    public Process() {
+    Process() {
     }
 
-    public Process(BigDecimal price, String currency, LocalDateTime buy_date,String description,String transaction_type, String notes, CategoryType categoryType, String ownerId) {
-        this.price = price;
-        this.currency = currency;
-        this.buy_date = buy_date;
-        this.description = description;
-        this.transaction_type = transaction_type;
-        this.notes = notes;
-        this.category = categoryType;
-        this.ownerId = ownerId;
+    private Process(final ProcessBuilder processBuilder) {
+        this.price = processBuilder.price;
+        this.currency = processBuilder.currency;
+        this.buy_date = processBuilder.buy_date;
+        this.description = processBuilder.description;
+        this.transaction_type = processBuilder.transaction_type;
+        this.notes = processBuilder.notes;
+        this.category = processBuilder.categoryType;
+        this.ownerId = processBuilder.ownerId;
     }
 
     public Long getId() {return id;}
 
     public BigDecimal getPrice() {return price;}
-    public void setPrice(final BigDecimal price) {this.price = price;}
+//    public void setPrice(final BigDecimal price) {this.price = price;}
 
     public String getCurrency() {return currency;}
-    public void setCurrency(final String currency) {this.currency = currency;}
+//    public void setCurrency(final String currency) {this.currency = currency;}
 
     public LocalDateTime getBuy_date() {return buy_date;}
-    public void setBuy_date(final LocalDateTime buy_date) {this.buy_date = buy_date;}
+//    public void setBuy_date(final LocalDateTime buy_date) {this.buy_date = buy_date;}
 
     public String getDescription() {return description;}
-    public void setDescription(final String description) {this.description = description;}
+//    public void setDescription(final String description) {this.description = description;}
 
     public String getTransaction_type() {return transaction_type;}
-    public void setTransaction_type(final String transaction_type) {this.transaction_type = transaction_type;}
+//    public void setTransaction_type(final String transaction_type) {this.transaction_type = transaction_type;}
 
     public String getNotes() {return notes;}
-    public void setNotes(final String notes) {this.notes = notes;}
+//    public void setNotes(final String notes) {this.notes = notes;}
 
     public String getOwnerId() {return ownerId;}
     public void setOwnerId(final String ownerId) {this.ownerId = ownerId;}
@@ -94,6 +94,58 @@ public class Process extends RepresentationModel<Process> {
         this.description = toUpdate.description;
         this.transaction_type = toUpdate.transaction_type;
         this.notes = toUpdate.notes;
+    }
+
+    public static class ProcessBuilder {
+        @NotBlank(message = "description can't be null or empty")
+        private String description;
+        @NotNull(message = "price can't be null or empty")
+        private BigDecimal price;
+        @NotBlank(message = "select currency")
+        private String currency;
+        private String transaction_type;
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        private LocalDateTime buy_date;
+        private String notes;
+        private CategoryType categoryType;
+        private String ownerId;
+
+        public ProcessBuilder buildDescription(final String description) {
+            this.description = description;
+            return this;
+        }
+        public ProcessBuilder buildPrice(final BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+        public ProcessBuilder buildCurrency(final String currency) {
+            this.currency = currency;
+            return this;
+        }
+        public ProcessBuilder buildTransactionType(final String transaction_type) {
+            this.transaction_type = transaction_type;
+            return this;
+        }
+        public ProcessBuilder buildBuyDate(final LocalDateTime buy_date) {
+            this.buy_date = buy_date;
+            return this;
+        }
+        public ProcessBuilder buildNotes(final String notes) {
+            this.notes = notes;
+            return this;
+        }
+        public ProcessBuilder buildCategoryType(final CategoryType categoryType) {
+            this.categoryType = categoryType;
+            return this;
+        }
+        public ProcessBuilder buildOwnerId(final String ownerId) {
+            this.ownerId = ownerId;
+            return this;
+        }
+
+        public Process build() {
+            return new Process(this);
+        }
     }
 }
 
--- src/main/resources/db/migration/V2__test_examples.sql
+++ src/main/resources/db/migration/V2__test_examples.sql
@@ -1,6 +1,17 @@
+INSERT INTO year_expenses (year, description, owner_id)
+VALUES ('2020', 'mock description 1', 'd2bc314b-d729-459c-b99d-37180d483012');
+
+INSERT INTO month_expenses (month, description, owner_id)
+VALUES ('january', 'mock month1', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       ('february', 'mock month2', 'd2bc314b-d729-459c-b99d-37180d483012');
+
+INSERT INTO category_expenses (type, description, owner_id)
+VALUES ('category1', 'mock description', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       ('category2', 'mock description222', 'd2bc314b-d729-459c-b99d-37180d483012');
+
 INSERT INTO expenses_process (price,currency,buy_date,description,transaction_type,notes,owner_id)
-VALUES (3000,'PLN','2020-03-12T12:30','silnik','by card','---','d2bc314b-d729-459c-b99d-37180d483012'),
-       (50.25,'USD','2022-05-22T09:30','skrzynia','by cash','---','d2bc314b-d729-459c-b99d-37180d483012'),
-       (3333.33,'EURO','2020-09-12T12:30','lusterko','by crypto','farcik','d2bc314b-d729-459c-b99d-37180d483012'),
-       (3333.39,'USD','2029-12-12T22:30','narty','by crypto','dobry wydatek','d2bc314b-d729-459c-b99d-37180d483012'),
-       (367344.33,'USD','2016-02-17T19:30','porsche','by card','za dolary','d2bc314b-d729-459c-b99d-37180d483012');
+VALUES (3000,'PLN','2020-03-12T12:30','silnik','by card','---', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       (50.25,'USD','2022-05-22T09:30','skrzynia','by cash','---', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       (3333.33,'EURO','2020-09-12T12:30','lusterko','by crypto','farcik', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       (3333.39,'USD','2029-12-12T22:30','narty','by crypto','dobry wydatek', 'd2bc314b-d729-459c-b99d-37180d483012'),
+       (367344.33,'USD','2016-02-17T19:30','porsche','by card','za dolary', 'd2bc314b-d729-459c-b99d-37180d483012');
